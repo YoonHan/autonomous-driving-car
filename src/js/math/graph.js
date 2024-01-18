09fa8fd9caf9914fd815603de404cc00e0568d6a
Yoon Han <yoontopia94@gmail.com>
2024-01-18 11:29:49 +0900

    feat: add draw method for Graph class

--- src/js/math/graph.js
+++ src/js/math/graph.js
@@ -1,1 +1,18 @@
-class Graph { }
+class Graph {
+  constructor(points = [], segments = []) {
+    this.points = points
+    this.segments = segments
+  }
+
+  draw(ctx) {
+    /** Draw segments first */
+    for (const seg of this.segments) {
+      seg.draw(ctx);
+    }
+
+    /** then draw points later (for overlapping segments) */
+    for (const point of this.points) {
+      point.draw(ctx);
+    }
+  }
+}
