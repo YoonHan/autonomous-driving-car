544376158f19f411b1196293a9c45eb75fc45955
Yoon Han <yoontopia94@gmail.com>
2024-01-22 15:55:42 +0900

    feat: add mousedown event listener

--- src/js/graph-editor.js
+++ src/js/graph-editor.js
@@ -1,3 +1,22 @@
 class GraphEditor {
-  constructor() { }
+  constructor(canvas, graph) {
+    this.canvas = canvas
+    this.graph = graph
+
+    this.ctx = this.canvas.getContext('2d')
+
+    this.#addEventListeners()
+  }
+
+  #addEventListeners() {
+    this.canvas.addEventListener('mousedown', ($event) => {
+      const { offsetX, offsetY } = $event
+      const clickedPoint = new Point(offsetX, offsetY)
+      this.graph.addPoint(clickedPoint)
+    })
+  }
+
+  display() {
+    this.graph.draw(this.ctx)
+  }
 }
