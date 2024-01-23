5cf8f96658dfc171dd65504141dd1feeb4cb0cfb
Yoon Han <yoontopia94@gmail.com>
2024-01-23 11:44:51 +0900

    feat: implement some user-friendly ux & do some refactoring

--- src/js/graph-editor.js
+++ src/js/graph-editor.js
@@ -6,6 +6,7 @@ class GraphEditor {
     this.graph = graph
     this.selectedPoint = null
     this.hoveredPoint = null
+    this.currentPoint = null
     this.isDragging = false
 
     this.ctx = this.canvas.getContext('2d')
@@ -14,50 +15,54 @@ class GraphEditor {
   }
 
   #addEventListeners() {
-    this.canvas.addEventListener('mouseup', () => {
-      this.isDragging = false
-    })
-
-    this.canvas.addEventListener('mousedown', ($event) => {
-      const { offsetX, offsetY, button } = $event
-      const clickedPoint = new Point(offsetX, offsetY)
-
-      if (button === MOUSE_EVENT_RIGHT_BUTTON_CODE) {
-        if (this.hoveredPoint) {
-          this.#removePoint(this.hoveredPoint)
-          return
-        }
-      }
-
-      if (button === MOUSE_EVENT_LEFT_BUTTON_CODE) {
-        if (this.hoveredPoint) {
-          this.selectedPoint = this.hoveredPoint
-          this.isDragging = true
-          return
-        }
-
-        this.graph.addPoint(clickedPoint)
-        this.selectedPoint = clickedPoint
-        this.hoveredPoint = clickedPoint
-      }
-    })
-
-
-    this.canvas.addEventListener('mousemove', ($event) => {
-      const { offsetX, offsetY } = $event
-      const currentPoint = new Point(offsetX, offsetY)
-
-      this.hoveredPoint = getNearestPoint(currentPoint, this.graph.points, 20)
-
-      if (this.isDragging) {
-        this.selectedPoint.x = offsetX
-        this.selectedPoint.y = offsetY
-      }
-    })
-
+    this.canvas.addEventListener('mouseup', this.#handleMouseUp.bind(this))
+    this.canvas.addEventListener('mousedown', this.#handleMouseDown.bind(this))
+    this.canvas.addEventListener('mousemove', this.#handleMouseMove.bind(this))
     this.canvas.addEventListener('contextmenu', ($event) => {
       $event.preventDefault()
     })
+  }
+
+  #handleMouseUp() {
+    this.isDragging = false
+  }
+
+  #handleMouseDown($event) {
+    if ($event.button === MOUSE_EVENT_RIGHT_BUTTON_CODE) {
+      if (this.selectedPoint) {
+        this.selectedPoint = null
+      } else if (this.hoveredPoint) {
+        this.#removePoint(this.hoveredPoint)
+      }
+    } else if ($event.button === MOUSE_EVENT_LEFT_BUTTON_CODE) {
+      if (this.hoveredPoint) {
+        this.#select(this.hoveredPoint)
+        this.isDragging = true
+        return
+      }
+
+      this.graph.addPoint(this.currentPoint)
+      this.#select(this.currentPoint)
+      this.hoveredPoint = this.currentPoint
+    }
+  }
+
+  #handleMouseMove($event) {
+    const { offsetX, offsetY } = $event
+    this.currentPoint = new Point(offsetX, offsetY)
+    this.hoveredPoint = getNearestPoint(this.currentPoint, this.graph.points, 20)
+
+    if (this.isDragging) {
+      this.selectedPoint.x = this.currentPoint.x
+      this.selectedPoint.y = this.currentPoint.y
+    }
+  }
+
+  #select(point) {
+    if (this.selectedPoint) {
+      this.graph.tryAddSegment(new Segment(this.selectedPoint, point))
+    }
+    this.selectedPoint = point
   }
 
   #removePoint(point) {
@@ -72,12 +77,14 @@ class GraphEditor {
 
   display() {
     this.graph.draw(this.ctx)
-    this.drawSelectedCircle()
+    this.drawCircleInPoint()
   }
 
-  drawSelectedCircle() {
+  drawCircleInPoint() {
     if (this.selectedPoint) {
+      const intent = this.hoveredPoint ? this.hoveredPoint : this.currentPoint
       this.selectedPoint.draw(this.ctx, { outline: true })
+      new Segment(this.selectedPoint, intent).draw(this.ctx, { dash: [3, 3] })
     }
 
     if (this.hoveredPoint) {
